ac3ee13a8ee50501825eb83fa3032c6f9041b30a
Erkan <ErkanOzulu@users.noreply.github.com>
2024-03-01 14:55:37 +0300

    eureka-user

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+//@FeignClient(value = "user-service",url = "http://localhost:9090") No need to use url, because eureka server(discovery server) manage the url part if there are more than one user-service instance, it automatically gives us available instance
+@FeignClient(value = "user-service")
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
