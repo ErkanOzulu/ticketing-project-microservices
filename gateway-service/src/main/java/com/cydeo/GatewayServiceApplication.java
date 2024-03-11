6c1be62084ec5f468bb906b480d1b4f40856dd2f
Erkan <ErkanOzulu@users.noreply.github.com>
2024-03-11 17:06:27 +0300

    swagger

--- gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
@@ -1,8 +1,16 @@
 package com.cydeo;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -12,6 +20,24 @@ public class GatewayServiceApplication {
 
         SpringApplication.run(GatewayServiceApplication.class);
     }
+
+    @Bean
+    public List<GroupedOpenApi> apiList(SwaggerUiConfigParameters swaggerUiConfigParameters,
+                                        RouteDefinitionLocator routeDefinitionLocator){
+        List<GroupedOpenApi> groupedOpenApis = new ArrayList<>();
+        List<RouteDefinition> definitions = routeDefinitionLocator
+                .getRouteDefinitions().collectList().block();
+
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")
+        ).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service","");
+            swaggerUiConfigParameters.addGroup(name);
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groupedOpenApis;
+    }
+
+
     //java way to customize predicate (predicates - Path=/cydeo/dev/user/**) instead of routes syntax in application.yml
     //    @Bean
 //    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
